bc980d02474e78948dca23ce7e702af18021f65b
Daniel Urdaneta <danielurn@hotmail.com>
2022-08-22 15:29:03 -0400

    Process env port added

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import axieapis from "./routes/axieapis.js";
 
-const PORT = 8080
+const PORT = process.env.PORT || 8080
 const app = express()
 
 app.listen(PORT,(err)=>{
